9d7f55ca8d3b83466af0c130ec16435783931725
wilbert1011 <wilber1011@hotmail.com>
2023-04-19 15:09:01 -0500

    primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/plantilla')
+
+@section('titulo','crud laravel 8')
+
+@section('contenido')
+<div class="row">
+    <h1>hola</h1>
+    <i class="fa-solid fa-star"></i><i class="fa-solid fa-user"></i>
+
+</div>
+@endsection
