61332cccfbc99ba2f04954d8f524358411a6dbc8
oficina <wilber1011@hotmail.com>
2023-04-19 19:21:27 -0500

    hdm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
 <div class="row">
     <h1>hola</h1>
     <h1>ramas</h1>
+    <h1>desde el <i class="fas fa-hospital    "></i></h1>
     <i class="fa-solid fa-star"></i><i class="fa-solid fa-user"></i>
+    
 
     
 
